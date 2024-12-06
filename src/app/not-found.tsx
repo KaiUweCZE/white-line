68a9d9a6bc16b3c78d1854c8fb06e1a7979d937f
Kai Uwe <pisezdenekpise@seznam.cz>
2024-12-06 16:30:11 +0100

    feat(error-handling): Add custom 404 Not Found page
    - Add custom 404 page with responsive design
    - Improve user experience for invalid routes
    - Maintain consistent branding and navigation

---
+++ src/app/not-found.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import corgiImg from "@/assets/images/corgi.webp";
+import Image from "next/image";
+import Link from "next/link";
+import Button from "@/ui/primitives/button";
+import { Home } from "lucide-react";
+
+const NotFound = () => {
+
+  
+  return (
+    <div className="container-error h-screen">
+        <section>
+      <Image
+        className="error-image"
+        src={corgiImg}
+        alt="catoon obrázek s corgi, která má u sebe otazník"
+        width={400}
+      />
+      <div className="flex flex-col w-full items-center">
+        <h2>Jéje, zatoulali jste se...</h2>
+        <Link href={"/"}>
+        <Button variant="outlined" leftIcon={<Home className="w-4 h-4"/>}>Zpět</Button>
+        </Link>
+      </div>
+      </section>
+    </div>
+  );
+};
+
+export default NotFound;
