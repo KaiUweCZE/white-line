29c9954a902f1cea2411e5603906ce7a6a4c2171
Kai Uwe <pisezdenekpise@seznam.cz>
2025-02-20 09:58:11 +0100

    feat(ui): use scroll animation for stuck navigatiion, hash navigation for articles

--- src/ui/articles/primary-article-view.tsx
+++ src/ui/articles/primary-article-view.tsx
@@ -1,8 +1,8 @@
-import { useContext } from "react";
-import Button from "../primitives/button";
-import Tag from "../primitives/tag";
-import { Article } from "./types";
-import { ArticleContext } from "@/context/article-context";
+import { useContext } from 'react';
+import Button from '../primitives/button';
+import Tag from '../primitives/tag';
+import { Article } from './types';
+import { ArticleContext } from '@/context/article-context';
 
 const PrimaryArticleView = (article: Article) => {
   const context = useContext(ArticleContext);
@@ -12,35 +12,33 @@ const PrimaryArticleView = (article: Article) => {
   const isActive = currentArticle.time === article.time;
   const outlinedClass = () => {
     switch (article.tags?.type) {
-      case "puppies":
-        return "outline outline-1 outline-fuchsia-300 bg-fuchsia-300/30";
-      case "races":
-        return "outline outline-1 outline-green-300 bg-green-300/30";
-      case "exhibition":
-        return "outline outline-1 outline-yellow-300 bg-yellow-300/20";
+      case 'puppies':
+        return 'outline outline-1 outline-fuchsia-300 bg-fuchsia-300/30';
+      case 'races':
+        return 'outline outline-1 outline-green-300 bg-green-300/30';
+      case 'exhibition':
+        return 'outline outline-1 outline-yellow-300 bg-yellow-300/20';
     }
   };
 
   const handleSetArticle = () => {
-    console.log("Set article", article);
+    window.location.hash = '';
+    console.log('Set article', article);
     setCurrentArticle(article);
+    window.location.hash = `current-article`;
   };
 
   return (
     <article
       className={`article-view relative shadow rounded p-4 hover:outline hover:outline-1 hover:outline-blue-300 transition-all duration-300
-     ${isActive ? outlinedClass() : "bg-white"}`}
+     ${isActive ? outlinedClass() : 'bg-white'}`}
     >
       <h3 className="font-semibold">{article.headline}</h3>
       <Tag variant={article.tags?.type}>{article.tags?.name}</Tag>
       <p className="line-clamp-2">{article.text}...</p>
       <footer className="flex justify-between">
         <time className="text-xs">{article.time}</time>
-        <Button
-          size="sm"
-          animation="undeline"
-          onClick={handleSetArticle}
-        >
+        <Button size="sm" animation="undeline" onClick={handleSetArticle}>
           Více
         </Button>
       </footer>
--- src/ui/menu/navbar.tsx
+++ src/ui/menu/navbar.tsx
@@ -1,12 +1,12 @@
-import { slides, mobileSlides } from "./data/menu-data";
-import NavbarMenu from "./navbar-menu";
-import Slideshow from "./slide-show";
+import { slides, mobileSlides } from './data/menu-data';
+import NavbarMenu from './navbar-menu';
+import Slideshow from './slide-show';
 
 const Navbar = () => {
   return (
     <header className="navbar" role="banner">
       <Slideshow slides={slides} mobileSlides={mobileSlides} />
-      <div className="header-bar  bg-stone-950/70">
+      <div className="header-bar bg-stone-950/70">
         <h1 className="flex pl-4 items-center" aria-label="logo">
           <span className="text-2xl font-bold">Whiteline</span>
           <span className="text-sky-400 text-2xl font-bold">Czech</span>
