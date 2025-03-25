9baf5cb761699709c5e7eaeb4017e84135238ff4
Kai Uwe <pisezdenekpise@seznam.cz>
2025-03-25 09:22:46 +0100

    older version

--- next.config.ts
+++ next.config.ts
@@ -1,35 +1,7 @@
-// next.config.ts
 import type { NextConfig } from 'next';
-import withBundleAnalyzer from '@next/bundle-analyzer';
-
-const bundleAnalyzer = withBundleAnalyzer({
-  enabled: process.env.ANALYZE === 'true',
-});
 
 const nextConfig: NextConfig = {
-  images: {
-    formats: ['image/avif', 'image/webp'],
-  },
-
-  webpack: (config, { isServer, dev }) => {
-    if (!dev && !isServer) {
-      config.optimization.splitChunks = {
-        chunks: 'all',
-        maxInitialRequests: 25,
-        minSize: 20000,
-      };
-
-      // Vylepšení pro opětovné využití modulů
-      config.optimization.moduleIds = 'deterministic';
-      config.optimization.chunkIds = 'deterministic';
-    }
-    return config;
-  },
-
-  // Experimentální features
-  experimental: {
-    optimizeCss: true,
-  },
+  /* config options here */
 };
 
-export default bundleAnalyzer(nextConfig);
+export default nextConfig;
